b43e78f7285b19c7c3467c8e6cc3c91ab89e8deb
valmojr <val_mo_jr@hotmail.com>
2023-11-22 01:35:33 -0300

    added initial dashboard and login routes

---
+++ src/app/(routes)/dashboard/layout.tsx
@@ -0,0 +1,8 @@
+export default function DashboardLayout({ children }: { children: React.ReactNode}) {
+    return (
+        <>
+            <h1>Dashboard Layout</h1>
+            {children}
+        </>
+    )
+}
