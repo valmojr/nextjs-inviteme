52eb9efdbf0b3c69f58e0600d120ce2976319a65
valmojr <val_mo_jr@hotmail.com>
2023-11-26 23:40:54 -0300

    added radix ui theme to the father layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,35 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Invite Me',
-  description: 'Cross Platform Inviting App',
-}
+	title: 'Invite Me',
+	description: 'Inviting App',
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Theme
+					appearance="dark"
+					accentColor="blue"
+					grayColor="sage"
+					className={
+						'flex flex-col flex-nowrap h-screen w-screen items-center justify-center'
+					}
+				>
+					{children}
+				</Theme>
+			</body>
+		</html>
+	);
 }
