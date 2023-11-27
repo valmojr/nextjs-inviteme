39bf471ba66f22fb60760390ada0a287653ac527
valmojr <val_mo_jr@hotmail.com>
2023-11-26 23:44:39 -0300

    added dashboard layout

--- src/app/(routes)/dashboard/layout.tsx
+++ src/app/(routes)/dashboard/layout.tsx
@@ -1,12 +1,12 @@
-import { loggedUser as mockUser } from "@/app/functions/mock/mock_data";
+import { loggedUser as mockUser } from '@/app/functions/mock/mock_data';
+import Sidebar from '@/app/ui/dashboard/Sidebar';
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-    const loggedUser = mockUser;
+export default function DashboardLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	const loggedUser = mockUser;
 
-    return (
-        <>
-            <h1>{`Logged as ${loggedUser.name}`}</h1>
-            {children}
-        </>
-    )
+	return <>{children}</>;
 }
