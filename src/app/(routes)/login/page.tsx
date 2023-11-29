ad63212ae72ff029b9c95784bd0f21e21a9dcf49
valmojr <val_mo_jr@hotmail.com>
2023-11-29 12:56:32 -0300

    added Tab component handler, and testing it into the login page (WIP)

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -1,3 +1,30 @@
+import Tab from '@/app/ui/util/tabs/Tab';
+
 export default function Page() {
-	return <h1>LOGIN PAGE</h1>;
+	return (
+		<>
+			<Tab.TabRoot>
+				<Tab.TabList>
+					<Tab.TabListItem contentRef="login">Login</Tab.TabListItem>
+					<Tab.TabListItem contentRef="register">
+						Register
+					</Tab.TabListItem>
+					<Tab.TabListItem contentRef="forgot">
+						Forgot Password
+					</Tab.TabListItem>
+				</Tab.TabList>
+				<Tab.TabContent>
+					<Tab.TabContentItem contentRef="login">
+						<h1>Login</h1>
+					</Tab.TabContentItem>
+					<Tab.TabContentItem contentRef="register">
+						<h1>Register</h1>
+					</Tab.TabContentItem>
+					<Tab.TabContentItem contentRef="forgot">
+						<h1>Forgot Password</h1>
+					</Tab.TabContentItem>
+				</Tab.TabContent>
+			</Tab.TabRoot>
+		</>
+	);
 }
