c1f2fa5bf66ca7361a7a8e5258b2c3b0f10bb472
valmojr <val_mo_jr@hotmail.com>
2023-12-01 12:56:39 -0300

    added initial register page

--- src/app/(routes)/login/register/page.tsx
+++ src/app/(routes)/login/register/page.tsx
@@ -1,4 +1,7 @@
 'use client';
+import SubmitButton from '@/app/ui/util/Buttons/SubmitButtom';
+import Input from '@/app/ui/util/Forms/Input';
+import Label from '@/app/ui/util/Forms/Label';
 import Paragrath from '@/app/ui/util/Text/Paragrath';
 import ThirdTitle from '@/app/ui/util/Text/ThirdTitle';
 import { useState } from 'react';
@@ -10,7 +13,19 @@ export default function Page() {
 
 	return (
 		<>
-			<Paragrath>Create your Account</Paragrath>
+			<fieldset>
+				<Label>Username</Label>
+				<Input></Input>
+			</fieldset>
+			<fieldset>
+				<Label>Password</Label>
+				<Input></Input>
+			</fieldset>
+			<fieldset>
+				<Label>Confirm Password</Label>
+				<Input></Input>
+			</fieldset>
+			<SubmitButton color={'primary'}>Register</SubmitButton>
 		</>
 	);
 }
