170e033e8b8a53cc8c17d4b059c0b1d4ceb95cd7
valmojr <val_mo_jr@hotmail.com>
2023-12-01 02:56:03 -0300

    added Submit Buttom component

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -1,10 +1,21 @@
-'use client';
-
 import SubmitButton from '@/app/ui/util/Buttons/SubmitButtom';
-import { useRouter } from 'next/navigation';
+import Input from '@/app/ui/util/Forms/Input';
+import Label from '@/app/ui/util/Forms/Label';
+import Paragrath from '@/app/ui/util/Text/Paragrath';
 
 export default function Page() {
-	const router = useRouter();
-
-	return <h1 className="text-black">Login Page</h1>;
+	return (
+		<>
+			<Paragrath>{'Insert your login info'}</Paragrath>
+			<fieldset className="Fieldset">
+				<Label htmlFor="name">Username</Label>
+				<Input id="name" defaultValue={'Pedro Duarte'} />
+			</fieldset>
+			<fieldset className="Fieldset">
+				<Label htmlFor="username">Password</Label>
+				<Input id="username" defaultValue={'@peduarte'} />
+			</fieldset>
+			<SubmitButton color={'primary'}>Save Changes</SubmitButton>
+		</>
+	);
 }
--- src/app/ui/util/Buttons/SubmitButtom.tsx
+++ src/app/ui/util/Buttons/SubmitButtom.tsx
@@ -3,15 +3,21 @@ import { twMerge } from 'tailwind-merge';
 
 export default function SubmitButton(
 	props: {
-		type?: 'primary' | 'secondary' | 'success' | 'error';
+		color?: 'primary' | 'secondary' | 'success' | 'danger';
 	} & ComponentProps<'button'>
 ) {
-	const { type } = props;
+	const { color } = props;
 
 	return (
 		<button
 			className={twMerge(
-				'Button green inline-flex items-center justify-center rounded-4px px-15 font-size-15 line-height-1 font-weight-500 h-35px bg-green-4 text-green-11 hover:bg-green-5 focus:shadow-outline-green-7',
+				'inline-flex items-center justify-center rounded-md px-4 py-2 font-size-15 line-height-1 font-weight-500 h-35 bg-green-4 text-green-11 hover:bg-green-5 focus:shadow-outline-green-7 cursor-pointer shadow-sm shadow-neutral-400',
+				color == 'primary' || color == undefined
+					? 'bg-blue-500 text-neutral-100'
+					: null,
+				color == 'secondary' ? 'bg-neutral-200 text-neutral-800' : '',
+				color == 'success' ? 'bg-emerald-600' : '',
+				color == 'danger' ? 'bg-red-500' : '',
 				props.className
 			)}
 		>
