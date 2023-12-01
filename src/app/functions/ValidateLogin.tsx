5939d8ef300893a80d9bf4fc7b5f658a3e4b8df8
valmojr <val_mo_jr@hotmail.com>
2023-12-01 12:56:53 -0300

    added validateLogin function

---
+++ src/app/functions/ValidateLogin.tsx
@@ -0,0 +1,21 @@
+import { User } from '@prisma/client';
+import { UserDTO } from './entities/User/User';
+
+export async function validateLogin(loginData: {
+	username: string;
+	password: string;
+}): Promise<User> {
+	const userDTO = new UserDTO();
+
+	const fetchedUsername = await userDTO.getByName(loginData.username);
+
+	if (!fetchedUsername) {
+		throw new Error('User not found');
+	}
+
+	if (fetchedUsername.password === loginData.password) {
+		return fetchedUsername;
+	} else {
+		throw new Error('Wrong password provided');
+	}
+}
