1d41bf5ad0e795ba9a623f053019c24d64ed182c
valmojr <val_mo_jr@hotmail.com>
2023-12-01 03:42:54 -0300

    removed radix theme, just wont be used

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
+import { twMerge } from 'tailwind-merge';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,17 +19,13 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
-				<Theme
-					appearance="dark"
-					accentColor="blue"
-					grayColor="sage"
-					className={
-						'flex flex-row flex-wrap h-screen w-screen items-center justify-center bg-gradient-to-r from-neutral-700 via-neutral-500 to-neutral-400 text-neutral-800'
-					}
-				>
-					{children}
-				</Theme>
+			<body
+				className={twMerge(
+					'flex flex-row flex-wrap h-screen w-screen items-center justify-center bg-gradient-to-r from-neutral-700 via-neutral-500 to-neutral-400 text-neutral-800',
+					inter.className
+				)}
+			>
+				{children}
 			</body>
 		</html>
 	);
