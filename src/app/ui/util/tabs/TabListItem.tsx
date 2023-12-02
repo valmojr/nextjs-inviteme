36a2ce22b09c83ed7dad175cfb2f91442ae95652
valmojr <val_mo_jr@hotmail.com>
2023-12-02 14:55:05 -0300

    refactored tablistitem component

--- src/app/ui/util/tabs/TabListItem.tsx
+++ src/app/ui/util/tabs/TabListItem.tsx
@@ -12,9 +12,7 @@ export default function TabListItem(
 	} & ComponentProps<'div'>
 ) {
 	const router = useRouter();
-
 	const localRoute = usePathname();
-
 	const isSelected = !!(localRoute == props.route);
 
 	const extractSegments = (urlPath: string): string | null => {
@@ -30,7 +28,7 @@ export default function TabListItem(
 	return (
 		<div
 			className={twMerge(
-				'flex-1 first:rounded-tl-xl last:rounded-tr-xl h-[45px] flex items-center justify-center bg-white font-size-15 line-height-1 text-mauve-11 select-none text-black',
+				'flex-1 first:rounded-tl-xl last:rounded-tr-xl h-[45px] flex items-center justify-center bg-white font-size-15 line-height-1 text-mauve-11 select-none text-black  shadow-md shadow-neutral-400 hover:shadow-2xl hover:shadow-neutral-800',
 				isSelected ? 'bg-blue-600' : 'hover:bg-blue-300',
 				props.className
 			)}
