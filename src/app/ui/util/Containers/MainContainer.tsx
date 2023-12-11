51e67cb7f8889a736b56454659e758ea71886b9c
valmojr <val_mo_jr@hotmail.com>
2023-12-10 23:16:58 -0300

    added MainContainer, reusable division component

---
+++ src/app/ui/util/Containers/MainContainer.tsx
@@ -0,0 +1,25 @@
+import { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+export default function MainContainer(
+	props: ComponentProps<'div'> & {
+		color?: 'primary' | 'secondary';
+	}
+) {
+	const { color } = props;
+
+	return (
+		<div
+			className={twMerge(
+				'flex flex-col flex-nowrap items-center justify-center',
+				color === 'primary' ? 'bg-neutral-700' : '',
+				color === 'secondary' ? 'bg-neutral-100' : '',
+				color === undefined ? '' : '',
+				props.className
+			)}
+			{...props}
+		>
+			{props.children}
+		</div>
+	);
+}
