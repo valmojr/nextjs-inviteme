28e301ce212724e51acb6f8c2228f7bc013cd6f2
valmojr <val_mo_jr@hotmail.com>
2023-12-13 00:43:04 -0300

    improved SubmitButtom style

--- src/app/ui/util/Buttons/SubmitButtom.tsx
+++ src/app/ui/util/Buttons/SubmitButtom.tsx
@@ -11,13 +11,19 @@ export default function SubmitButton(
 	return (
 		<button
 			className={twMerge(
-				'inline-flex items-center justify-center rounded-md px-4 py-2 font-size-15 line-height-1 font-weight-500 h-35 bg-green-4 text-green-11 hover:bg-green-5 focus:shadow-outline-green-7 cursor-pointer shadow-sm shadow-neutral-400',
+				'inline-flex items-center justify-center rounded-md px-4 py-2 font-semibold line-height-2 font-weight-500 h-35 bg-green-4 text-green-11 hover:bg-green-5 focus:shadow-outline-green-7 cursor-pointer shadow-sm shadow-neutral-400 active:translate-y-1 duration-75 hover:shadow-md active:shadow-xl',
 				color == 'primary' || color == undefined
-					? 'bg-blue-500 text-neutral-100'
-					: null,
-				color == 'secondary' ? 'bg-neutral-200 text-neutral-800' : '',
-				color == 'success' ? 'bg-emerald-600 text-neutral-200' : '',
-				color == 'danger' ? 'bg-red-500 text-neutral-200' : '',
+					? 'bg-blue-500 text-neutral-100 hover:bg-blue-600'
+					: '',
+				color == 'secondary'
+					? 'bg-neutral-200 text-neutral-800 hover:bg-neutral-300'
+					: '',
+				color == 'success'
+					? 'bg-emerald-600 text-neutral-200 hover:bg-emerald-700'
+					: '',
+				color == 'danger'
+					? 'bg-red-500 text-neutral-200 hover:bg-red-600'
+					: '',
 				props.className
 			)}
 			{...props}
