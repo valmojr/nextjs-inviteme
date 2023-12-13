eac0d42a58b51137b8f9df01cf03444de4d12507
valmojr <val_mo_jr@hotmail.com>
2023-12-13 08:13:44 -0300

    added get code properties from the url in server component

--- src/app/(routes)/login/redirect/page.tsx
+++ src/app/(routes)/login/redirect/page.tsx
@@ -1,15 +1,15 @@
-'use client';
-import { useSearchParams } from 'next/navigation';
 import GetUser from './GetUser';
+import { headers } from 'next/headers';
 
 export default function RedirectPage() {
-	const code = `${useSearchParams().get('code')}`;
+	const headersList = headers().get('referer') || '';
+	const urlObject = new URL(headersList);
+	const codeValue = urlObject.searchParams.get('code') || '';
 
 	return (
 		<>
 			<h1>Redirecting...</h1>
-			<h3>{code}</h3>
-			<GetUser code={code} />
+			<GetUser code={codeValue} />
 		</>
 	);
 }
