00947e8039696e2c819ca6793d3e8205e0b75f52
valmojr <val_mo_jr@hotmail.com>
2023-12-13 07:35:42 -0300

    styled avatar component

--- src/app/ui/util/avatar.tsx
+++ src/app/ui/util/avatar.tsx
@@ -1,5 +1,7 @@
+import { User } from '@prisma/client';
 import Image from 'next/image';
 import { ComponentProps, Suspense } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 export function FallbackAvatar() {
 	return <h1>Loading...</h1>;
@@ -7,21 +9,84 @@ export function FallbackAvatar() {
 
 export default function Avatar(
 	props: {
-		image: string;
+		image?: string;
+		profile?: User;
 		border?: 'circle' | 'rounded' | 'square';
 		size?: 'small' | 'medium' | 'large';
 		alt?: string;
 	} & ComponentProps<'img'>
 ) {
-	const { image, border, size, alt } = props;
+	let { image, profile, border, size, alt } = props;
 
-	return (
-		<Image
-			src={image}
-			width={300}
-			height={300}
-			alt={alt ? alt : ''}
-			className="w-16 h-16 my-2 rounded-full select-none"
-		/>
-	);
+	if (profile && !profile?.avatar) {
+		profile.avatar = '';
+	}
+
+	if (image) {
+		return (
+			<Image
+				src={image}
+				width={300}
+				height={300}
+				alt={alt ? alt : ''}
+				className={twMerge(
+					'my-2 select-none shadow-md hover:shadow-xl',
+					border === 'circle' ? 'rounded-full' : null,
+					border === 'rounded' || border === undefined
+						? 'rounded-xl'
+						: null,
+					border === 'square' ? 'rounded-none' : null,
+					size === 'small' ? 'w-8 h-8' : null,
+					size === 'medium' || size === undefined
+						? 'w-16 h-16'
+						: null,
+					size === 'large' ? 'w-20 h-20' : null
+				)}
+			/>
+		);
+	} else if (profile && typeof profile.avatar == 'string') {
+		return (
+			<Image
+				src={profile.avatar}
+				width={300}
+				height={300}
+				alt={alt ? alt : ''}
+				className={twMerge(
+					'my-2 select-none shadow-md hover:shadow-xl',
+					border === 'circle' ? 'rounded-full' : null,
+					border === 'rounded' || border === undefined
+						? 'rounded-xl'
+						: null,
+					border === 'square' ? 'rounded-none' : null,
+					size === 'small' ? 'w-8 h-8' : null,
+					size === 'medium' || size === undefined
+						? 'w-16 h-16'
+						: null,
+					size === 'large' ? 'w-20 h-20' : null
+				)}
+			/>
+		);
+	} else {
+		return (
+			<Image
+				src={''}
+				width={300}
+				height={300}
+				alt={alt ? alt : ''}
+				className={twMerge(
+					'my-2 select-none shadow-md hover:shadow-xl',
+					border === 'circle' ? 'rounded-full' : null,
+					border === 'rounded' || border === undefined
+						? 'rounded-xl'
+						: null,
+					border === 'square' ? 'rounded-none' : null,
+					size === 'small' ? 'w-8 h-8' : null,
+					size === 'medium' || size === undefined
+						? 'w-16 h-16'
+						: null,
+					size === 'large' ? 'w-20 h-20' : null
+				)}
+			/>
+		);
+	}
 }
