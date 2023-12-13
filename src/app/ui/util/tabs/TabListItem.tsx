3c11db652e9ed7f86ab72645a0eb23e7f53f3bbc
valmojr <val_mo_jr@hotmail.com>
2023-12-13 14:03:10 -0300

    added cursor pointer to tab buttons

--- src/app/ui/util/tabs/TabListItem.tsx
+++ src/app/ui/util/tabs/TabListItem.tsx
@@ -29,7 +29,7 @@ export default function TabListItem(
 		<div
 			className={twMerge(
 				'flex-1 first:rounded-tl-xl last:rounded-tr-xl h-[45px] flex items-center justify-center bg-white font-size-15 line-height-1 text-mauve-11 select-none text-black  shadow-md shadow-neutral-400 hover:shadow-2xl hover:shadow-neutral-800',
-				isSelected ? 'bg-blue-600' : 'hover:bg-blue-300',
+				isSelected ? 'bg-blue-600' : 'hover:bg-blue-300 cursor-pointer',
 				props.className
 			)}
 			onClick={() => router.push(props.route)}
