55f2971f11a2a2ff85e7b6cce50fcb5ddb5a65d8
valmojr <val_mo_jr@hotmail.com>
2023-12-14 20:51:32 -0300

    GetUser component for the OAuth2 authetication is actually getting the user info from the Discord API

--- src/app/(routes)/login/redirect/GetUser.tsx
+++ src/app/(routes)/login/redirect/GetUser.tsx
@@ -8,11 +8,6 @@ export default async function GetUserInfo({ code }: { code: string }) {
 
 	return (
 		<>
-			<h1>
-				{'CLIENT ID: ' +
-					JSON.stringify(process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID)}
-			</h1>
-			<h1>{'RESPONSE: ' + JSON.stringify(response)}</h1>
 			<h1>{'USER: ' + JSON.stringify(user)}</h1>
 		</>
 	);
