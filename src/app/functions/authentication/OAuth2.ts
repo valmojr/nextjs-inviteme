4da4b018576d4a3274440ef4b7994da90e683f0e
valmojr <val_mo_jr@hotmail.com>
2023-12-14 10:29:13 -0300

    added OAuth2 authentication functions

---
+++ src/app/functions/authentication/OAuth2.ts
@@ -0,0 +1,44 @@
+export type TokenResponse = {
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+    refresh_token: string;
+    scope: string;
+}
+
+export async function GetToken(code: string) {
+    const api_url = 'https://discord.com/api/oauth2/token';
+    const body = new URLSearchParams();
+    body.append('client_id', process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID || '');
+    body.append('client_secret', process.env.DISCORD_CLIENT_SECRET || '');
+    body.append('grant_type', 'authorization_code');
+    body.append('code', code);
+    body.append('redirect_uri', process.env.NEXT_PUBLIC_DISCORD_REDIRECT_URI || '');
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    
+    const response = await fetch(api_url, {
+        method: 'POST',
+        headers,
+        body,
+    });
+
+    const token = await response.json() as TokenResponse;
+    
+    return token;
+}
+
+export async function GetUser({access_token}: TokenResponse) {
+    const api_url = 'https://discord.com/api/users/@me';
+    const headers = new Headers();
+    headers.append('Authorization', `Bearer ${access_token}`);
+
+    const response = await fetch(api_url, {
+        method: 'GET',
+        headers,
+    });
+
+    const user = await response.json();
+    
+    return user;
+}
