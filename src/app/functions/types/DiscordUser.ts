5e032f9a7a8621b6dd196c1c1fce6fec1ca339c9
valmojr <val_mo_jr@hotmail.com>
2023-12-14 10:50:55 -0300

    added DiscordUser type

---
+++ src/app/functions/types/DiscordUser.ts
@@ -0,0 +1,20 @@
+type DiscordUser = {
+    id:string,
+    username:string,
+    avatar:string,
+    discriminator:string,
+    public_flags:number,
+    premium_type:number,
+    flags:number,
+    banner:string,
+    accent_color:number,
+    global_name:string,
+    avatar_decoration_data:string,
+    banner_color:string,
+    mfa_enabled:false,
+    locale:string,
+    email:string,
+    verified:true
+}
+
+export default DiscordUser;
