3354eb056823b67a72d170eb982e9fbb28c63e98
valmojr <val_mo_jr@hotmail.com>
2023-12-16 00:08:19 -0300

    GetUser Component is throwing JSON Web Token after fetching the User on Discord API

--- src/app/(routes)/login/redirect/GetUser.tsx
+++ src/app/(routes)/login/redirect/GetUser.tsx
@@ -1,14 +1,13 @@
-'use strict';
-
+import GenerateJWT from '@/app/functions/authentication/GenerateJWT';
 import { GetToken, GetUser } from '@/app/functions/authentication/OAuth2';
 
 export default async function GetUserInfo({ code }: { code: string }) {
 	const response = await GetToken(code);
 	const user = await GetUser(response);
-
+	const jwt = await GenerateJWT(user);
 	return (
 		<>
-			<h1>{'USER: ' + JSON.stringify(user)}</h1>
+			<h1>{'JWT: ' + jwt}</h1>
 		</>
 	);
 }
