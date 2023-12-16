08ffb7bf6e309a714d63df60fdd86ad7769bb818
valmojr <val_mo_jr@hotmail.com>
2023-12-16 14:41:08 -0300

    adding JWT token generation to GetUser component

--- src/app/(routes)/login/redirect/GetUser.tsx
+++ src/app/(routes)/login/redirect/GetUser.tsx
@@ -1,10 +1,16 @@
+'use server';
+
 import GenerateJWT from '@/app/functions/authentication/GenerateJWT';
 import { GetToken, GetUser } from '@/app/functions/authentication/OAuth2';
+import { cookies } from 'next/headers';
 
 export default async function GetUserInfo({ code }: { code: string }) {
 	const response = await GetToken(code);
 	const user = await GetUser(response);
 	const jwt = await GenerateJWT(user);
+
+	cookies().set('token', jwt, { secure: true });
+
 	return (
 		<>
 			<h1>{'JWT: ' + jwt}</h1>
