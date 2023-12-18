81414c1dea57e2a033f6943e311695487fd67d79
valmojr <val_mo_jr@hotmail.com>
2023-12-18 09:58:01 -0300

    added initial event page, dynamic route

---
+++ src/app/(routes)/event/[id]/Page.tsx
@@ -0,0 +1,7 @@
+import Paragrath from '@/app/ui/util/Text/Paragrath';
+
+export default function Page() {
+	<Paragrath>
+		This is the Event Page, it will show events, based on their id on URL
+	</Paragrath>;
+}
