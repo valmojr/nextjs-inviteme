ffdebe17505064b511ad485a2399c469178c7215
valmojr <val_mo_jr@hotmail.com>
2023-12-19 09:06:38 -0300

    deleted getUser component, will be using the dynamic route with the provided code by the Discord OAuth2

--- src/app/(routes)/login/redirect/GetCode.tsx
+++ src/app/(routes)/login/redirect/GetCode.tsx
@@ -1,16 +1,19 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import GetUser from './GetUser';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function GetCode() {
 	const url = useSearchParams();
 	const code = new URLSearchParams(url).get('code') || '';
+	const router = useRouter();
+	useEffect(() => {
+		async function generateToken() {
+			router.push('/login/redirect/callback/' + code);
+		}
 
-	return (
-		<>
-			<GetUser code={code} />
-		</>
-	);
+		generateToken();
+	}, [code, router]);
+
+	return <></>;
 }
