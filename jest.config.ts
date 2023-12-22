792a8321f0d1ea53393ac0ad3ac04ebb36119925
valmojr <val_mo_jr@hotmail.com>
2023-12-22 10:24:42 -0300

    configured Jest for typescript

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,14 @@
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 
 const config: Config = {
-    verbose: false,
-    collectCoverage: true,
-    extensionsToTreatAsEsm: ['.ts'],
+	verbose: false,
+	collectCoverage: true,
+	extensionsToTreatAsEsm: ['.ts'],
+	transformIgnorePatterns: ['/node_modules/'],
+	transform: {
+		'^.+\\.tsx?$': 'ts-jest', // assuming TypeScript
+		'^.+\\.jsx?$': 'babel-jest',
+	},
 };
 
 export default config;
