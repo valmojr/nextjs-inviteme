656f89c634a31a0ac713840f1da52c2787f15d5c
valmojr <val_mo_jr@hotmail.com>
2023-12-23 12:21:46 -0300

    forced SWC on next config, Jest uses Babel

--- next.config.js
+++ next.config.js
@@ -9,7 +9,10 @@ const nextConfig = {
                 pathname: '/**'
             }
         ],
-    }
+    },
+    experimental: {
+        forceSwcTransforms: true,
+    },
 }
 
 module.exports = nextConfig
