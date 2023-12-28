cdacb2cc2f1477e26f0adf605ad2b2c572fcdc1b
valmojr <val_mo_jr@hotmail.com>
2023-12-28 00:22:27 -0300

    added jwt authentication route

---
+++ src/app/api/auth/check/route.ts
@@ -0,0 +1,38 @@
+import HttpResponse from '@/app/functions/API/HttpResponses';
+import { verify } from 'jsonwebtoken';
+import { cookies } from 'next/headers';
+import { NextRequest } from 'next/server';
+
+async function Handler(req: NextRequest) {
+	const jwt = cookies().get('token');
+	if (req.method !== 'POST') {
+		return new HttpResponse().UnauthorizedMethod();
+	}
+	if (!jwt) {
+		return new HttpResponse().Unauthorized();
+	} else {
+		const jsonwebtoken = verify(
+			`${jwt}`,
+			process.env.JWT_SECRET as string
+		) as string;
+		if (!jsonwebtoken) {
+			return new HttpResponse().Unauthorized();
+		}
+
+		const { user } = JSON.parse(jsonwebtoken);
+
+		return new HttpResponse().Ok({
+			status: 200,
+			message: 'authorized',
+			user,
+		});
+	}
+}
+
+export {
+	Handler as GET,
+	Handler as POST,
+	Handler as PUT,
+	Handler as PATCH,
+	Handler as DELETE,
+};
