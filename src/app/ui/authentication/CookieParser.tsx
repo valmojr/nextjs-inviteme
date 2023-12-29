c7ec7c65ed9e2f279532706ce6bada6ce6ca1311
valmojr <val_mo_jr@hotmail.com>
2023-12-29 12:36:55 -0300

    finished cookie parser component to handle cookie handling

--- src/app/ui/authentication/CookieParser.tsx
+++ src/app/ui/authentication/CookieParser.tsx
@@ -1,24 +1,30 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { cookies as CookieHandler } from 'next/headers';
+import { useEffect } from 'react';
+import { CookiesProvider, useCookies } from 'react-cookie';
 import Paragrath from '../util/Text/Paragrath';
+import { redirect } from 'next/navigation';
 
 export default function CookieParser({ cookies }: { cookies: string }) {
-	const [cookied, setCookied] = useState<boolean>(false);
 	const jwt = cookies.split(';')[0].split('=')[1];
+	const [fetchedCookies, setCookie] = useCookies(['token']);
 
 	useEffect(() => {
-		if (typeof window !== 'undefined') {
-			localStorage.setItem('token', jwt);
+		setCookie('token', jwt, { path: '/', sameSite: true, secure: true });
+	}, [setCookie, jwt]);
 
-			localStorage.getItem('token')
-				? setCookied(true)
-				: setCookied(false);
-		}
-	}, [cookies, jwt]);
+	if (fetchedCookies) {
+		redirect('/dashboard');
+	} else {
+		throw new Error('Cookie not found');
+	}
 
 	return (
-		<Paragrath className={'text-center'}>
-			{(cookied ? 'sem cookie\n' : 'com cookie\n') + jwt}
-		</Paragrath>
+		<>
+			<CookiesProvider />
+			<Paragrath>
+				{fetchedCookies ? 'redirecting...' : 'sem cookie'}
+			</Paragrath>
+		</>
 	);
 }
