7f88b1076e59e7c2fd2307f995d28735135da594
valmojr <val_mo_jr@hotmail.com>
2023-12-29 12:36:26 -0300

    added route bifurc on root route to login or dashboard route, depending if the user is logged or not

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-'use client';
-import { useRouter } from 'next/router';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
+	cookies().get('token') ? redirect('/dashboard') : redirect('/login');
+
 	return <></>;
 }
