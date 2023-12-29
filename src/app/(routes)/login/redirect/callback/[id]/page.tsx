c660a7ed134763cb8b1b7badfef22df077b86a44
valmojr <val_mo_jr@hotmail.com>
2023-12-29 04:30:44 -0300

    finished discord Oauth2 callback page

--- src/app/(routes)/login/redirect/callback/[id]/page.tsx
+++ src/app/(routes)/login/redirect/callback/[id]/page.tsx
@@ -1,19 +1,51 @@
-import GenerateJWT from '@/app/functions/authentication/GenerateJWT';
-import { GetToken, GetUser } from '@/app/functions/authentication/OAuth2';
+import HttpResponses from '@/app/functions/API/HttpResponses';
+import {
+	GetToken,
+	GetUser,
+} from '@/app/functions/authentication/DiscordOAuth2';
+import DiscordUser from '@/app/functions/types/DiscordUser';
+import DiscordAvatarParser from '@/app/functions/util/DiscordAvatarParser';
+import CookieParser from '@/app/ui/authentication/CookieParser';
 import Avatar from '@/app/ui/util/Avatar';
+import Paragrath from '@/app/ui/util/Text/Paragrath';
 import SecondTitle from '@/app/ui/util/Text/SecondTitle';
 
 export default async function Page({ params }: { params: { id: string } }) {
 	const access_info = await GetToken(params.id);
-	const user = await GetUser(access_info);
-	const jwt = GenerateJWT(user);
+	const {
+		id,
+		username,
+		avatar,
+		email,
+		banner_color,
+		global_name,
+	}: DiscordUser = await GetUser(access_info);
+	const url = `${process.env.ENVIRONMENT_URI}/api/auth/discord/me`;
+
+	const response = await fetch(url, {
+		method: 'POST',
+		body: JSON.stringify({
+			user: { id, username, avatar, email, banner_color, global_name },
+		}),
+	});
+
+	if (!response.ok) {
+		throw new HttpResponses().BadRequest();
+	}
+
+	const fetchedCookies = response.headers.get('set-cookie');
 
 	return (
 		<>
-			<Avatar profile={user} size={'large'} border={'circle'} />
+			<Avatar
+				image={DiscordAvatarParser(id || '', avatar)}
+				size={'large'}
+				border={'circle'}
+			/>
 			<SecondTitle
 				className={'text-center'}
-			>{`Logged in as ${user.global_name}!`}</SecondTitle>
+			>{`Logged in as ${global_name}!`}</SecondTitle>
+			<CookieParser cookies={fetchedCookies as string} />
 		</>
 	);
 }
