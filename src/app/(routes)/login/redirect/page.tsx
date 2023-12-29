e8dfb4a3dd9830d1ade46cea7610158319347a2d
valmojr <val_mo_jr@hotmail.com>
2023-12-29 04:33:18 -0300

    fixed redirect route

--- src/app/(routes)/login/redirect/page.tsx
+++ src/app/(routes)/login/redirect/page.tsx
@@ -1,7 +1,8 @@
 import FirstTitle from '@/app/ui/util/Text/FirstTitle';
-import GetCode from './GetCode';
+import GetCode from '../../../ui/authentication/GetCode';
+import SecondTitle from '@/app/ui/util/Text/SecondTitle';
 
-export default function RedirectPage() {
+export default function RedirectPage({ params }: { params: { code: string } }) {
 	return (
 		<>
 			<FirstTitle>Redirecting...</FirstTitle>
