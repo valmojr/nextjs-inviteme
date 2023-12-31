d45dfe628f26c976b3cb0e596b1776ae45f8ac72
valmojr <val_mo_jr@hotmail.com>
2023-12-30 21:34:18 -0300

    added sidebar component to dashboard page

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import AvaliableEvents from '@/app/ui/dashboard/AvaliableEvents';
 import IncomingEvents from '@/app/ui/dashboard/IncomingEvents';
+import Sidebar from '@/app/ui/dashboard/Sidebar';
 
 export default function Page() {
 	return (
 		<>
+			<Sidebar />
 			<IncomingEvents />
 			<AvaliableEvents />
 		</>
