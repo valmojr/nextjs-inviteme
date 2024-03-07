da6213490babed86d65af21bdd46a941fe28be2f
valmojr <val_mo_jr@hotmail.com>
2024-03-06 22:23:32 -0300

    used upsertUser function in discord login route

--- src/app/api/auth/discord/me/route.ts
+++ src/app/api/auth/discord/me/route.ts
@@ -1,87 +1,78 @@
-import { cookies } from 'next/headers';
-import { sign } from 'jsonwebtoken';
-import { User } from '@prisma/client';
-import StreamToBuffer from '@/app/functions/util/GetStreamData';
-import HttpResponse from '@/app/functions/API/HttpResponses';
-import { randomUUID } from 'crypto';
-import DiscordUser from '@/app/functions/types/DiscordUser';
-import { createUser, getUserByDiscordId } from '@/app/functions/entities/User';
+import { cookies } from "next/headers";
+import { sign } from "jsonwebtoken";
+import { User } from "@prisma/client";
+import StreamToBuffer from "@/app/functions/util/GetStreamData";
+import HttpResponse from "@/app/functions/API/HttpResponses";
+import { randomUUID } from "crypto";
+import DiscordUser from "@/app/functions/types/DiscordUser";
+import { upsertUser } from "@/app/functions/entities/User";
 
 function userCheck(user: any) {
-	if (!user) return false;
-	if (!user?.id) return false;
-	if (!user?.username) return false;
-	if (!user?.avatar) return false;
-	if (!user?.email) return false;
-	if (!user?.banner_color) return false;
-	if (!user?.global_name) return false;
-	return true;
+  if (!user) return false;
+  if (!user?.id) return false;
+  if (!user?.username) return false;
+  if (!user?.avatar) return false;
+  if (!user?.email) return false;
+  if (!user?.banner_color) return false;
+  if (!user?.global_name) return false;
+  return true;
 }
 
 async function handler(req: Request) {
-	const body = JSON.parse(await StreamToBuffer(req?.body));
+  const body = JSON.parse(await StreamToBuffer(req?.body));
 
-	if (!userCheck(body?.user)) {
-		return new HttpResponse().BadRequest();
+  if (!userCheck(body?.user)) {
+    return new HttpResponse().BadRequest('no user provided on body');
+  }
+
+  const fetchedUser: DiscordUser = body?.user;
+
+  const userData: User = {
+    id: randomUUID(),
+    discordId: fetchedUser.id,
+    username: fetchedUser.username,
+    avatar: fetchedUser.avatar,
+    email: fetchedUser.email,
+    bannerColor: fetchedUser.banner_color,
+    displayName: fetchedUser.global_name,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    password: null,
+  };
+
+  const user = await upsertUser(userData);
+
+	if (!user) {
+		throw new HttpResponse().InternalServerError();
 	}
 
-	const fetchedUser: DiscordUser = body?.user;
+  const jwt = sign(user || "", process.env.AUTH_SECRET as string);
 
-	const userData: User = {
-		id: randomUUID(),
-		discordId: fetchedUser.id,
-		username: fetchedUser.username,
-		avatar: fetchedUser.avatar,
-		email: fetchedUser.email,
-		bannerColor: fetchedUser.banner_color,
-		displayName: fetchedUser.global_name,
-		createdAt: new Date(),
-		updatedAt: new Date(),
-		password: null,
-	};
+  cookies().set("token", jwt, {
+    path: "/",
+    httpOnly: false,
+    sameSite: "strict",
+    maxAge: 31536000,
+    secure: true,
+  });
 
-	let user: User | undefined;
+  const response = new HttpResponse().Ok({
+    message: "User logged in",
+    user: user,
+  });
 
-	try {
-		user = await getUserByDiscordId(fetchedUser.id) as User;
-
-		if (!user) {
-			user = await createUser(userData);
-			if (!user) {
-				throw new Error('Failed to upsert user');
-			}
-		}
-		console.log(user);
-	} catch (error) {
-		console.error(error);
-	}
-
-	const jwt = sign(user || '', process.env.AUTH_SECRET as string);
-
-	cookies().set('token', jwt, {
-		path: '/',
-		httpOnly: false,
-		sameSite: 'strict',
-		maxAge: 31536000,
-		secure: true,
-	});
-
-	const response = new HttpResponse().Ok({
-		user,
-	});
-
-	return response;
+  return response;
 }
 
-export const POST = handler;
 
 function UnauthorizedMethod() {
-	return new HttpResponse().UnauthorizedMethod();
+  return new HttpResponse().UnauthorizedMethod();
 }
 
 export {
+	handler as POST,
 	UnauthorizedMethod as GET,
-	UnauthorizedMethod as PUT,
-	UnauthorizedMethod as PATCH,
-	UnauthorizedMethod as DELETE,
+  UnauthorizedMethod as PUT,
+  UnauthorizedMethod as PATCH,
+  UnauthorizedMethod as DELETE,
 };
