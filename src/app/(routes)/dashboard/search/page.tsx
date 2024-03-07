4ea7040e4c39a99a8903c7b28d9d7a71e83b5e88
valmojr <val_mo_jr@hotmail.com>
2024-03-06 22:20:53 -0300

    added container for search content in search route

--- src/app/(routes)/dashboard/search/page.tsx
+++ src/app/(routes)/dashboard/search/page.tsx
@@ -1,26 +1,25 @@
-import SearchResults from '@/app/ui/dashboard/search/SearchResults';
-import Searcher from '@/app/ui/dashboard/search/Searcher';
-import ContentContainer from '@/app/ui/util/Divisions/ContentContainer';
-import ScreenContainer from '@/app/ui/util/Divisions/ScreenContainer';
-import { Suspense } from 'react';
+import SearchResults from "@/app/ui/dashboard/search/SearchResults";
+import Searcher from "@/app/ui/dashboard/search/Searcher";
+import ContentContainer from "@/app/ui/util/Divisions/ContentContainer";
+import ScreenContainer from "@/app/ui/util/Divisions/ScreenContainer";
+import { Suspense } from "react";
 
 export default function Page({
-	searchParams,
+  searchParams,
 }: {
-	searchParams?: { query?: string, type: | 'event' | 'user' | 'house'};
+  searchParams?: { query?: string; type: "event" | "user" | "house" };
 }) {
-	const query = searchParams?.query || '';
-	const type = searchParams?.type || 'event';
+  const query = searchParams?.query || "";
+  const type = searchParams?.type || "event";
 
-	return (
-		<ScreenContainer className={'justify-start p-4'}>
-			<ContentContainer>
-				<Searcher placeholder="Search" />
-			</ContentContainer>
-            
-			<Suspense fallback={<div>Loading...</div>}>
-				<SearchResults search={query} type={type} />
-			</Suspense>
-		</ScreenContainer>
-	);
+  return (
+    <ScreenContainer className={"justify-start p-4"}>
+      <ContentContainer>
+        <Searcher placeholder="Search" />
+      </ContentContainer>
+      <ContentContainer className={'w-full h-[615px] justify-start items-center'}>
+          <SearchResults search={query} type={type} />
+      </ContentContainer>
+    </ScreenContainer>
+  );
 }
