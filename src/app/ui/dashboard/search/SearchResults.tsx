969402c968878b921a967fb8a51bf3f78d706a13
valmojr <val_mo_jr@hotmail.com>
2024-03-15 15:06:34 -0300

    prevented Search Route to fetch on empty search params

--- src/app/ui/dashboard/search/SearchResults.tsx
+++ src/app/ui/dashboard/search/SearchResults.tsx
@@ -11,46 +11,47 @@ export default async function SearchData({
   search: string;
   type: "event" | "user" | "house";
 }) {
-  const token = cookies().get("token")?.value;
-  const response = await fetch(`${process.env.BACKEND_URI}/api/${type}/partial/${search}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
-	const data: Event[] | House[] | User[] = [];
-
-	try {
-		const results = (await response.json());		
-	
-		for (const result of results) {
-			data.push(result);
-		}
-	} catch (error) {
-		console.log(error);
-	}
-
-  if (data.length > 0) {
-    return (
-      <ResultFrame>
-        {data.map((result: Event | House | User) => (
-          <>
-            <Suspense fallback={<ResultContainerSkeleton />}>
-              <ResultContainer key={result.id} result={result} />
-            </Suspense>
-          </>
-        ))}
-      </ResultFrame>
+  if (search.length > 0) {
+    const token = cookies().get("token")?.value;
+    const response = await fetch(
+      `${process.env.BACKEND_URI}/api/${type}/partial/${search}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
     );
+    const data: Event[] | House[] | User[] = [];
+
+    try {
+      const results = await response.json();
+
+      for (const result of results) {
+        data.push(result);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    if (data.length > 0) {
+      return (
+        <ResultFrame>
+          {data.map((result: Event | House | User) => (
+            <>
+              <Suspense fallback={<ResultContainerSkeleton />}>
+                <ResultContainer key={result.id} result={result} />
+              </Suspense>
+            </>
+          ))}
+        </ResultFrame>
+      );
+    }
   }
 
-	if (search.length == 0) {
-		return (
-				<h1 className={"mt-8 text-gray-500"}>{"Search for something"}</h1>
-		);
-	}
-  return (
-      <h1 className={"mt-8 text-gray-500"}>{`No ${type} results found`}</h1>
-  );
+  if (search.length == 0) {
+    return <h1 className={"mt-8 text-gray-500"}>{"Search for something"}</h1>;
+  }
+  return <h1 className={"mt-8 text-gray-500"}>{`No ${type} results found`}</h1>;
 }
