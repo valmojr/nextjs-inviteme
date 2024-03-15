c140653b3870af68e2e21244c9beeba04f1c8c81
valmojr <val_mo_jr@hotmail.com>
2024-03-15 20:41:14 -0300

    refactored houses viewer in profile route

--- src/app/ui/dashboard/Profile/ProfileHouses.tsx
+++ src/app/ui/dashboard/Profile/ProfileHouses.tsx
@@ -5,10 +5,15 @@ import { cookies } from "next/headers";
 import Link from "next/link";
 import Avatar from "../../util/Avatar";
 import { Suspense } from "react";
-import HouseIcon from '../../../../../public/image/house.svg';
+import HouseIcon from "../../../../../public/image/house.svg";
+import ErrorBoundary from "../../util/Error/ErrorBoundary";
+import { twMerge } from "tailwind-merge";
+import Row from "../../util/Divisions/Row";
+import Col from "../../util/Divisions/Col";
+import ProfileHouseContainer from "./ProfileHouseContainer";
 
 export default async function ProfileHouses() {
-  const token = cookies().get('token')?.value;
+  const token = cookies().get("token")?.value;
 
   const profile = Verify(token as string) as User;
 
@@ -18,11 +23,31 @@ export default async function ProfileHouses() {
     house.avatar = house.avatar ? house.avatar : HouseIcon;
   });
 
-  return <Suspense fallback={<div/>}>
-    {houses.map((house) => 
-      <Link href={''} key={house.id}>
-        <Avatar image={house.avatar as string} size={'small'} border={'circle'} />
-      </Link>
-    )}
-  </Suspense>
+  return (
+    <Suspense
+      fallback={
+        <>
+          <SuspendedAvatar />
+          <SuspendedAvatar />
+          <SuspendedAvatar />
+        </>
+      }
+    >
+      <Row className={'gap-1 flex-wrap'}>
+        {houses.map((house) => (
+          <ProfileHouseContainer key={house.id} house={house} />
+        ))}
+      </Row>
+    </Suspense>
+  );
+}
+
+function SuspendedAvatar() {
+  return (
+    <div
+      className={twMerge(
+        "h-8 w-8 rounded-full bg-stone-600 animate-pulse 2s infinite"
+      )}
+    />
+  );
 }
