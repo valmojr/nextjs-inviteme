bbdf556f19d76e31593a274587e5bb3813b2a9df
valmojr <val_mo_jr@hotmail.com>
2024-03-25 17:30:18 -0300

    randomized result skeletons ids

--- src/app/ui/dashboard/search/ResultFrame.tsx
+++ src/app/ui/dashboard/search/ResultFrame.tsx
@@ -1,13 +1,14 @@
 import { ReactElement, Suspense } from "react";
 import { ResultContainerSkeleton } from "./ResultContainer";
 import { twMerge } from "tailwind-merge";
+import { randomUUID } from "crypto";
 
 export default function ResultFrame({children, className}: {children: ReactElement[], className?: string}) {
   return <div className={twMerge('flex flex-col flex-nowrap gap-3 w-full py-1', className)}>
     <Suspense fallback={<>
-      <ResultContainerSkeleton key={1} />
-      <ResultContainerSkeleton key={2} />
-      <ResultContainerSkeleton key={3} />
+      <ResultContainerSkeleton key={randomUUID()} />
+      <ResultContainerSkeleton key={randomUUID()} />
+      <ResultContainerSkeleton key={randomUUID()} />
     </>}>
       {children}
     </Suspense>
