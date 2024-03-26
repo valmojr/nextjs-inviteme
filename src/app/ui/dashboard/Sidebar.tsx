0fa6773826be0b5c8e8fd02567d9723706815303
valmojr <val_mo_jr@hotmail.com>
2024-03-26 12:03:13 -0300

    updated sidebar token verify type

--- src/app/ui/dashboard/Sidebar.tsx
+++ src/app/ui/dashboard/Sidebar.tsx
@@ -1,17 +1,17 @@
-import { User } from '@prisma/client';
-import { cookies } from 'next/headers';
-import Avatar from '../util/Avatar';
-import SecondTitle from '../util/Text/SecondTitle';
-import Verify from '@/app/functions/authentication/Verify';
+import { User } from "@prisma/client";
+import { cookies } from "next/headers";
+import Avatar from "../util/Avatar";
+import SecondTitle from "../util/Text/SecondTitle";
+import Verify from "@/app/functions/authentication/Verify";
 export default async function Sidebar() {
-	const token = cookies().get('token')?.value as string;
+  const token = cookies().get("token")?.value as string;
 
-	const loggedUser = Verify(token) as User;
+  const { user } = Verify(token);
 
-	return (
-		<div className="h-14 w-screen top-0 bg-neutral-900 text-white flex flex-row flex-nowrap justify-center items-center gap-4 shadow-neutral-500 shadow-lg">
-			<SecondTitle>{`${loggedUser.displayName?.toUpperCase()}`}</SecondTitle>
-			<Avatar profile={loggedUser} size={'small'} border={'circle'} />
-		</div>
-	);
+  return (
+    <div className="h-14 w-screen top-0 bg-neutral-900 text-white flex flex-row flex-nowrap justify-center items-center gap-4 shadow-neutral-500 shadow-lg">
+      <SecondTitle>{`${user.displayName?.toUpperCase()}`}</SecondTitle>
+      <Avatar profile={user} size={"small"} border={"circle"} />
+    </div>
+  );
 }
