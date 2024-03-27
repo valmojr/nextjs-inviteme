61099ebeb0020ca540832a568598df297dfa9ffd
valmojr <val_mo_jr@hotmail.com>
2024-03-27 19:03:44 -0300

    removed suspense api from result frame (div only, no fetch here)

--- src/app/ui/dashboard/search/ResultFrame.tsx
+++ src/app/ui/dashboard/search/ResultFrame.tsx
@@ -1,16 +1,8 @@
-import { ReactElement, Suspense } from "react";
-import { ResultContainerSkeleton } from "./ResultContainer";
+import { ReactElement } from "react";
 import { twMerge } from "tailwind-merge";
-import { randomUUID } from "crypto";
 
-export default function ResultFrame({children, className}: {children: ReactElement[], className?: string}) {
+export default function ResultFrame({children, className}: {children: ReactElement[] | ReactElement, className?: string}) {
   return <div className={twMerge('flex flex-col flex-nowrap gap-3 w-full py-1', className)}>
-    <Suspense fallback={<>
-      <ResultContainerSkeleton key={randomUUID()} />
-      <ResultContainerSkeleton key={randomUUID()} />
-      <ResultContainerSkeleton key={randomUUID()} />
-    </>}>
       {children}
-    </Suspense>
   </div>
 }
