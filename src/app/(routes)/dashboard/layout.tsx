42fb5b319481f858c5280f8ba9c30cd6fc2a3992
valmojr <val_mo_jr@hotmail.com>
2024-03-30 00:39:59 -0300

    refactoring forms

--- src/app/(routes)/dashboard/layout.tsx
+++ src/app/(routes)/dashboard/layout.tsx
@@ -1,6 +1,4 @@
 import SideMenu from "@/app/ui/dashboard/SideMenu/SideMenu";
-import Sidebar from "@/app/ui/dashboard/Sidebar";
-import Col from "@/app/ui/util/Divisions/Col";
 import ContentContainer from "@/app/ui/util/Divisions/ContentContainer";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
