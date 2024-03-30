a0dc972eed36cbab9ff9b981110fd8bcd66ef043
valmojr <val_mo_jr@hotmail.com>
2024-03-30 00:38:22 -0300

    refactored side menu

--- src/app/ui/dashboard/SideMenu/SideMenu.tsx
+++ src/app/ui/dashboard/SideMenu/SideMenu.tsx
@@ -1,30 +1,62 @@
 import { cn } from "@/lib/utils";
-import SideMenuButton from "./SideMenuButton";
 import { cookies, headers } from "next/headers";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Verify from "@/app/functions/authentication/Verify";
+import { Castle, CirclePlus, CircleUser, Search } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import SideMenuButton from "./SideMenuButton";
+import { useRouter } from "next/navigation";
 
 export default function SideMenu() {
-  const url = headers().get("next-url");
   const token = cookies().get("token")?.value;
 
   const { user } = Verify(token);
 
+  const AvatarFallBackName = user.username[0].concat(
+    user.username[user.username.length - 1]
+  );
+
   return (
-    <div
+    <Card
       className={cn(
-        "fixed flex flex-row flex-nowrap bg-black h-12 w-screen bottom-0 justify-center items-center gap-",
-        "lg:relative lg:flex-col lg:p-2 lg:gap-2 lg:justify-start lg:w-72 lg:h-[400px] lg:shadow-lg lg:bg-stone-200 lg:rounded-lg",
+        "fixed flex flex-row flex-nowrap h-12 w-screen bottom-0 justify-center items-center gap-",
+        "lg:relative lg:flex-col lg:p-2 lg:gap-2 lg:justify-start lg:w-72 lg:h-fit lg:shadow-lg lg:rounded-lg"
       )}
     >
-      <Avatar>
-        <AvatarImage src={user.avatar || ''} />
-        <AvatarFallback>{user.displayName || 'VT'}</AvatarFallback>
-      </Avatar>
-      <SideMenuButton label="Create" icon="create" />
-      <SideMenuButton label="Home" icon="home" />
-      <SideMenuButton label="Search" icon="search" />
-      <SideMenuButton label="Profile" icon="profile" />
-    </div>
+      <Card className={cn("flex flex-row h-fit gap-0 w-full")}>
+        <CardHeader className={""}>
+          <Avatar className={"h-14 w-14"}>
+            <AvatarImage src={user.avatar || ""} alt={user.username} />
+            <AvatarFallback>{AvatarFallBackName}</AvatarFallback>
+          </Avatar>
+        </CardHeader>
+        <CardHeader>
+          <CardTitle>{user.displayName}</CardTitle>
+          <CardDescription>{user.username}</CardDescription>
+        </CardHeader>
+      </Card>
+      <SideMenuButton href={"/dashboard/create"}>
+        <CirclePlus strokeWidth={1.1} />
+        <h1>Create</h1>
+      </SideMenuButton>
+      <SideMenuButton href={"/dashboard"}>
+        <Castle strokeWidth={1.1} />
+        <h1>Home</h1>
+      </SideMenuButton>
+      <SideMenuButton href={"/dashboard/search"}>
+        <Search strokeWidth={1.1} />
+        <h1>Search</h1>
+      </SideMenuButton>
+      <SideMenuButton href={"/dashboard/profile"}>
+        <CircleUser strokeWidth={1.1} />
+        <h1>Profile</h1>
+      </SideMenuButton>
+    </Card>
   );
 }
