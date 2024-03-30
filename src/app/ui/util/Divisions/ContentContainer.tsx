3d5bf671a3a010c7433203f152d6e6ba1ed82dd4
valmojr <val_mo_jr@hotmail.com>
2024-03-30 00:37:55 -0300

    refaotered components to use shadcn cards

--- src/app/ui/util/Divisions/ContentContainer.tsx
+++ src/app/ui/util/Divisions/ContentContainer.tsx
@@ -1,21 +1,21 @@
+import { Card } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
 import { ComponentProps } from 'react';
 
 export default function ContentContainer(
 	props: { orientation?: 'row' | 'col' } & ComponentProps<'div'>
 ) {
-	const { className, orientation } = props;
+	const { className, children } = props;
 
 	return (
-		<div
-			{...props}
+		<Card
 			className={cn(
-				'flex flex-wrap items-center justify-center gap-3',
-				'drop-shadow-md bg-neutral-200 px-3 py-2 rounded-md',
-				'lg:w-[680px] h-[730px]',
-				orientation == 'row' ? 'flex-row' : 'flex-col',
+				'flex flex-wrap flex-col items-center justify-center gap-2',
+				'lg:w-[680px] lg:h-[750px]',
 				className
 			)}
-		></div>
+		>
+			{children}
+		</Card>
 	);
 }
