b787c6e6ca3687414b971861e89cab74ade35047
valmojr <val_mo_jr@hotmail.com>
2024-05-17 17:21:47 -0300

    removed unused components on search route

--- src/app/ui/dashboard/search/ResultContainers/EventResultContainer.tsx
+++
@@ -1,8 +0,0 @@
-import FirstTitle from "@/app/ui/util/Text/FirstTitle";
-import { Event } from "@prisma/client";
-
-export default function EventResultContainer({ event, className }: { event: Event, className?: string }) {
-  return <div className={className}>
-    <FirstTitle>{event.name}</FirstTitle>
-  </div>
-}
--- src/app/ui/dashboard/search/ResultFrame.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from "react";
-import { twMerge } from "tailwind-merge";
-
-export default function ResultFrame({children, className}: {children: ReactElement[] | ReactElement, className?: string}) {
-  return <div className={twMerge('flex flex-col flex-nowrap gap-3 w-full py-1', className)}>
-      {children}
-  </div>
-}
