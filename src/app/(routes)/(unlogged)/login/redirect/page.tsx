13de2afa1419172a1e2ad20a8fab9dd69da59950
valmojr <val_mo_jr@hotmail.com>
2024-05-19 12:26:35 -0300

    fixing suspense boundary wrapping in discord oauth2 login redirect

--- src/app/(routes)/(unlogged)/login/redirect/page.tsx
+++ src/app/(routes)/(unlogged)/login/redirect/page.tsx
@@ -1,5 +1,22 @@
+'use client';
+
 import FirstTitle from '@/app/ui/util/Text/FirstTitle';
-import GetCode from '../../../../ui/authentication/GetCode';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+function GetCode() {
+	const url = useSearchParams();
+	const code = new URLSearchParams(url).get('code') || '';
+	const router = useRouter();
+	useEffect(() => {
+		async function generateToken() {
+			router.push('/login/redirect/callback/' + code);
+		}
+		generateToken();
+	}, [code, router]);
+
+	return <></>;
+}
 
 export default function RedirectPage() {
 	return (
