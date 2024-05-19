769df0a1919fc18b5276c3a373411b3f3c2dccc7
valmojr <val_mo_jr@hotmail.com>
2024-05-19 00:21:01 -0300

    updated imports on root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { twMerge } from "tailwind-merge";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 
