51540750032d9c331dc8ba7f50eb103052c4de15
valmojr <val_mo_jr@hotmail.com>
2024-05-21 23:36:32 -0300

    fixed RightMenu margin

--- src/app/ui/dashboard/RightMenu/RightMenu.tsx
+++ src/app/ui/dashboard/RightMenu/RightMenu.tsx
@@ -3,10 +3,10 @@ import { cn } from "@/lib/utils";
 
 function RightMenu() {
   return (
-    <div className="h-screen mt-36 p-3 flex flex-col justify-start items-start">
+    <div className="h-full w-[450px] pt-20 p-3 pl-0">
       <Card
         className={cn(
-          "h-96 w-[300px]",
+          "h-96 w-full",
           "flex flex-col flex-nowrap justify-center items-center",
           "border dark:bg-zinc-900 bg-zinc-100"
         )}
