d4e9c8356fa5f5ad3244198302c77d55173d2cf8
valmojr <val_mo_jr@hotmail.com>
2024-05-22 03:52:11 -0300

    added dark theme to create entity layout

--- src/app/(routes)/(logged)/create/layout.tsx
+++ src/app/(routes)/(logged)/create/layout.tsx
@@ -4,11 +4,12 @@ import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 
 function CreateHousePage({ children }: { children: React.ReactNode }) {
+
   return (
     <Card
       className={cn(
         "flex flex-col flex-nowrap justify-center items-center",
-        "w-full h-full bg-zinc-900",
+        "w-full h-full dark:bg-zinc-900 bg-zinc-100",
         "border"
       )}
     >
