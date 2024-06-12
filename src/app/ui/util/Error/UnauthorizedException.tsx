8bc55028e2a685b85e38906c43c6a39f5ddde62c
valmojr <val_mo_jr@hotmail.com>
2024-06-12 08:08:30 -0300

    fixed grammar

--- src/app/ui/util/Error/UnauthorizedException.tsx
+++ src/app/ui/util/Error/UnauthorizedException.tsx
@@ -1,4 +1,4 @@
-export default function UnauthorizedExepctionComponent() {
+export default function UnauthorizedExceptionComponent() {
   return (
     <div className={
       'px-4 py-2 bg-red-600 color-white rounded-md shadow-md'
