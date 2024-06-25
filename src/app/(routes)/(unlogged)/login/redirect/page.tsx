a062963bbc792b63b203860cf656880e3421d990
valmojr <val_mo_jr@hotmail.com>
2024-06-25 03:25:06 -0300

    added more info to discord oauth2 page

--- src/app/(routes)/(unlogged)/login/redirect/page.tsx
+++ src/app/(routes)/(unlogged)/login/redirect/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { Progress } from "@/components/ui/progress";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { User } from "@prisma/client";
 import GenerateToken from "../../../../ui/authentication/GenerateToken";
 
 export default function RedirectPage() {
   const [progress, setProgress] = useState<number>(0);
+  const [message, setMessage] = useState<string>('Discord OAuth2 Login Provider');
   const [data, setData] = useState<any>();
   const url = useSearchParams();
   const code = new URLSearchParams(url).get("code") || "";
@@ -15,6 +15,7 @@ export default function RedirectPage() {
   useEffect(() => {
     async function fetchData() {
       setProgress(40);
+      setMessage('Fetching Discord User Info');
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_BACKEND_URI}/api/auth/discord/${code}`,
         {
@@ -23,9 +24,11 @@ export default function RedirectPage() {
       );
 
       if (response) setProgress(78);
-
+  
+      setMessage("Converting Response Provided");
       const data = await response.json();
-
+  
+      setMessage("extracted info provided => " + JSON.stringify(data));
       if (data?.user) setProgress(100);
 
       setData(data);
@@ -37,6 +40,7 @@ export default function RedirectPage() {
   return (
     <>
       <Progress value={progress} className="w-full mt-5" />
+      <h1>{message}</h1>
       {data?.token && <GenerateToken token={data?.token}/>}
     </>
   );
